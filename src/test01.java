5766fccaee1196fcbf01b6654b08a54da092ec91
Rohit <rohit.birdi2@gmail.com>
2019-10-16 09:10:28 +0200

    first commit from laptop

--- src/test01.java
+++ src/test01.java
@@ -1,9 +1,20 @@
-class HelloWorld
-{
-    // Your program begins with a call to main().
-    // Prints "Hello, World" to the terminal window.
-    public static void main(String[] args)
-    {
-        System.out.println("Hello World,it is me Rohit");
+package rohit.test01.java;
+
+//mport java.io.BufferedReader;
+//import java.io.InputStreamReader;
+//import java.io.IOException;
+
+//Enter console stuff and ausgeben
+public class test01 {
+    public static void main()//String[] args) //throws IOException
+    {   //var read = System.console().readLine();
+        //BufferedReader rd = new BufferedReader(new InputStreamReader(System.in));
+        //System.out.println("Enter a string");
+        //String s = rd.readLine();
+        //System.out.println("You entered :" + s);
+
     }
+
+
 }
+
---
+++ src/test02.java
@@ -0,0 +1,24 @@
+package rohit.test01.java;
+
+//import java.io.IOException;
+//import java.nio.charset.StandardCharsets;
+//import java.nio.file.Files;
+//import java.nio.file.Path;
+//import java.nio.file.Paths;
+//import java.util.List;
+
+//Lesen aus einer Textfile
+
+public class test02 {
+        public static void main()//String[] args) //throws IOException
+        {
+//test
+            //String fileName = "C:\\Users\\rohit\\IdeaProjects\\Test01\\test.txt";
+            //Path path = Paths.get(fileName);
+            //byte[] bytes = Files.readAllBytes(path);
+            //List<String> allLines = Files.readAllLines(path, StandardCharsets.UTF_8);
+            //for (var s : allLines)
+            //    System.out.println(s);
+        }
+    }
+
